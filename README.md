95153462fd63dfa2eb9d2db216560599f123a8e3
Anthony Stansbridge <github@anthonystansbridge.co.uk>
2016-05-24 15:52:54 +0100

    Rebuilding site Tuesday, May 24th 2016, 3:52:54 pm.

--- README.md
+++
@@ -1,5 +0,0 @@
-# Untangled
-"Tahuna sands" A.K.A Light Brown: #CFC194;
-"Aqua" A.K.A Light Blue: #A3E7D9;
-"Coral" A.K.A Orange: #FC7138;
-"Soya Bean": A.K.A Brown: #695D47;
--- tools/check.js
+++
@@ -1,9 +0,0 @@
-var Log = require('log');
-var shelljs = require('shelljs');
-var colors = require('colors');
-
-var logger = new Log('warning');
-if(!shelljs.which('hugo')) { 
-    logger.warning(colors.red('Hugo is required in $PATH, https://gohugo.io'));
-    shelljs.exit(1);
-}
