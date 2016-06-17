ee5c5f5c6aa1e05c46e364f5ac8dfd638b7f591f
Anthony Stansbridge <github@anthonystansbridge.co.uk>
2016-06-17 15:28:43 +0100

    Added build tools for CSS/JS minification

---
+++ tools/flush.js
@@ -0,0 +1,7 @@
+var Log = require('log');
+var logger = new Log('info');
+var shelljs = require('shelljs');
+logger.info(colors.yellow('Adding changes to git...'));
+shelljs.exec('git add -A');
+shelljs.exec('git commit -m "CSS/JS Build"');
+logger.info(colors.green('OK'));
