4dafc0e41c204f37505c250993cd28f20d15be2f
edpe <ed@edperkins.co.uk>
2019-04-26 13:57:07 +0100

    new untangled 2019 site build

--- tools/flush.js
+++
@@ -1,8 +0,0 @@
-var Log = require('log');
-var logger = new Log('info');
-var colors = require('colors');
-var shelljs = require('shelljs');
-logger.info(colors.yellow('Adding changes to git...'));
-shelljs.exec('git add -A');
-shelljs.exec('git commit -m "CSS/JS Build"');
-logger.info(colors.green('OK'));
